562decda08da700e28d310db2dc7a35a12b77e71
Bia <Bia@10.30.199.243>
2017-08-21 21:48:38 -0300

    Nova Versão

--- MyLogin/src/br/edu/facear/dao/ClienteDAO.java
+++ MyLogin/src/br/edu/facear/dao/ClienteDAO.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import br.edu.facear.model.Cliente;
 
@@ -12,6 +14,7 @@ public class ClienteDAO extends GenericDAO{
 	private String LOGIN_SQL = "SELECT * "
 							 + "FROM TBCLIENTE "
 			                 + "WHERE EMAIL = ? AND SENHA = ?;";
+	private String LISTAR_SQL="SELECT * FROM TBCLIENTE;";
 	
 	public Cliente autenticar(String email, String senha) throws ClassNotFoundException, IOException, SQLException{
 		Cliente c = null;
@@ -40,4 +43,54 @@ public class ClienteDAO extends GenericDAO{
 		
 		return c;
 	}
+	
+	
+	/**
+	 * Metodo de inserção de cliente
+	 */
+	
+	public void inserir(Cliente c) {
+		
+	}
+	/**
+	 * Metodo de alteração de cliente
+	 */
+	public void alterar(Cliente c ) {
+		
+	}/**
+	 * Metodo de exclusão de cliente
+	 */
+	public void excluir(Cliente c ) {
+		
+	}
+	
+	/**
+	 * Metodo de listagem  de todos os  clientes
+	 * @throws SQLException 
+	 * @throws IOException 
+	 * @throws ClassNotFoundException 
+	 */
+	public List<Cliente> listar( ) throws ClassNotFoundException, IOException, SQLException {
+		List<Cliente> lista = new ArrayList<Cliente>();
+		// Abriri conexão
+		openConnection();
+		// Executar script
+		ps=  connect.prepareStatement(LISTAR_SQL);
+		ResultSet  rs = ps.executeQuery();
+		//Analisar o retorno do script
+		if(rs!= null) {
+			while(rs.next()) {
+				Cliente c = new Cliente(rs.getInt("id"),
+										rs.getString("nome"),
+										rs.getString("cpf"),	
+										rs.getString("email"), 
+										rs.getString("senha"));
+				lista.add(c);
+			}
+		}
+		closeConnection();
+		return lista;
+	}
+	
+	
 }
